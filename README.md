150386d9b168c7c12a2b44195e2dd67d27360e0a
wesinator <5124946+wesinator@users.noreply.github.com>
2023-07-26 23:16:04 -0400

    work around https://crbug.com/116429
    v0.3.0

--- README.md
+++ README.md
@@ -14,21 +14,9 @@ Uses [crypto-api](https://github.com/nf404/crypto-api) v0.8.5 library.
 
 #### Known issues (specific to Chrome extension)
 
- - Chromium has a [stupid bug](https://crbug.com/1346156) where `alert()` messages containing multiline text cannot be copied out of the alert dialog. 
-   To workaround, open the console and copy the output of the hash from there 
-   
-   (sorry but for now I'm too lazy to make a fancy pageAction UI that displays the hash output)
-
- - Multiline text is currently hashed incorrectly because of another [longstanding bug](https://crbug.com/116429) with how `info.selectionText` does text conversion in Chrome (newlines are replaced with space characters for some reason). 
+ - The output currently shows the hash in an alert() dialog and tab console (there are some technical challenges to getting output in the extension popup to work).
  
- `document.documentElement.innerText` gives the correct expected text selection (for whole text page).
- 
-   These issues are not present with the Firefox extension.
+ Chromium has a [bug](https://crbug.com/958139) where `alert()` messages containing multiline text cannot be copied out of the alert dialog. 
+   To workaround, open the console and copy the output of the hash from there.
 
-#### Experience and remarks
-
-At time of development: 
-
-**TL,DR** the Firefox version is better and not buggy, because of differences in Firefox where it seems the Firefox API implementation is much more robust.
-
-having written the same extension functionality for Firefox and Chromium, even after a working port to Chromium manifest v3 implementation, Chome itself has been both buggier and more astonishing in how it presents and handles multiline text. Firefox is much less buggy and astonishing for functionality of this extension.
+   This issue is not present with the Firefox extension.
--- src/background.js
+++ src/background.js
@@ -1,45 +1,67 @@
 importScripts('./hash.js', './include/crypto-api.min.js')
 
-function displayHash(hashText, hashType, hash) {
+/* two executeScript calls are used in order to workaround https://bugs.chromium.org/p/chromium/issues/detail?id=116429
+the first calls sendHashMsg() to send the correct text selection to the extension background message listener. 
+The listener then sends the correct text selection to genHash() which runs the second executeScript to display the output in the active tab.
+*/
+
+// function to display string output in tab
+function displayInTab(string) {
+    console.log(string);
+    alert(string);
+}
+
+/*
+tab var is needed for tabId to display output
+*/
+function genHash(tab, hashText, hashType) {
+    var hash = null;
+    switch (hashType) {
+        case "md5":
+            hash = md5(hashText);
+            break;
+        case "sha1":
+            hash = sha1(hashText);
+            break;
+        case "sha256":
+            hash = sha256(hashText);
+            break;
+        case "sha512":
+            hash = sha512(hashText);
+            break;
+    }
+    
     // truncate long text String
     var selectionOutput = hashText.substring(0,4096);
       if (hashText.length > 4096)
           selectionOutput = selectionOutput + "...";
 
-    hashStr = `${hashType.toUpperCase()}: ${hash}\n\n${chrome.i18n.getMessage("TextHeading")}\n'${selectionOutput}'\n`; 
-    console.log(hashStr);
-    alert(hashStr);
+    hashStr = `${hashType.toUpperCase()}: ${hash}\n\n${chrome.i18n.getMessage("TextHeading")}\n'${selectionOutput}'\n`;
+    chrome.scripting.executeScript({
+        target: {tabId: tab.id},
+        func: displayInTab,
+        args: [hashStr]
+    });
 }
 
+function sendHashMsg(hashType) {
+    // https://developer.chrome.com/docs/extensions/mv3/messaging/
+    (async () => {
+        const response = await chrome.runtime.sendMessage({hashType: hashType, hashText: window.getSelection().toString()});
+        // do something with response here, not outside the function
+        //console.log(response);
+    })();
+}
 
 hashTheText = function(info, tab) {
-    var hashText = info.selectionText;
     var hashType = info.menuItemId;
-    var hash = null;
     
-    switch (hashType) {
-        case "md5":
-            hash = md5(info.selectionText);
-            break;
-        case "sha1":
-            hash = sha1(info.selectionText);
-            break;
-        case "sha256":
-            hash = sha256(info.selectionText);
-            break;
-        case "sha512":
-            hash = sha512(info.selectionText);
-            break;
-    }
-
-  if (hash) {
       // display the hash in browser alert
       chrome.scripting.executeScript({
           target: {tabId: tab.id},
-          func: displayHash,
-          args: [hashText, hashType, hash]
+          func: sendHashMsg,
+          args: [hashType]
       });
-  }
 };
 
 chrome.runtime.onInstalled.addListener(() => {
@@ -75,3 +97,16 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.contextMenus.onClicked.addListener(hashTheText);
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    console.log(sender.tab ?
+                "from a content script: " + sender.tab.url :
+                "from the extension");
+    var hashType = request.hashType;
+    if (hashType) {
+        genHash(sender.tab, request.hashText, request.hashType);
+        //sendResponse({farewell: "thanks for sending! goodbye"});
+    }
+  }
+);
